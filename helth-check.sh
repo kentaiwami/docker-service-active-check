a10d78f6664685fb8ad14718d2adf59ca0c1b6d9
Kenta Iwami <kentaiwami12@gmail.com>
2020-01-05 22:49:20 +0900

    portfolioとportfolio-redirectが重複してしまう問題を修正

--- helth-check.sh
+++ helth-check.sh
@@ -1,6 +1,6 @@
 . ./.env
 
-service_name_list=("sumolog" "shifree" "portfolio" "finote" "phpmyadmin" "letsencrypt" "proxy")
+service_name_list=("sumolog" "shifree" "portfolio" "portfolio-redirect" "finote" "phpmyadmin" "letsencrypt" "proxy")
 ok_list=()
 ng_list=()
 cause_list=()
@@ -31,9 +31,21 @@ send_notification() {
     curl -s -S -X POST -d "${payload}" ${SLACK_NOTIFICATION_URL} > /dev/null
 }
 
+run_docker_process_command() {
+    service_name=$1
+
+    if [ $service_name = "portfolio" ]; then
+        result_docker_process=$(docker ps -f "name=${service_name}" -f "status=running" --format "{{.Names}}\t{{.Status}}" | grep -v portfolio-redirect)
+    else
+        result_docker_process=$(docker ps -f "name=${service_name}" -f "status=running" --format "{{.Names}}\t{{.Status}}")
+    fi
+
+    echo "$result_docker_process"
+}
+
 for index in "${!service_name_list[@]}"; do
     service_name=${service_name_list[$index]}
-    result_docker_process=$(docker ps -f "name=${service_name}" -f "status=running" --format "{{.Names}}\t{{.Status}}")
+    result_docker_process=`run_docker_process_command ${service_name}`
     running_service_count=$(echo "$result_docker_process" | wc -l)
 
     # docker psの結果が空文字だったらng確定
@@ -42,7 +54,7 @@ for index in "${!service_name_list[@]}"; do
         cause_list+=($cause_docker_text)
     else
         # 稼働中のサービスの個数が一致していなければng
-        if [ $running_service_count = ${ACTIVE_SERVICE_COUNT_LIST[$index]} ]; then
+        if [[ $running_service_count = ${ACTIVE_SERVICE_COUNT_LIST[$index]} ]]; then
             ok_list+=($service_name)
         else
             ng_list+=($service_name)
