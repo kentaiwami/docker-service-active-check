93d803ce48af3c28aaf7101afa122d5a44c386ec
Kenta Iwami <kentaiwami12@gmail.com>
2020-01-15 01:27:41 +0900

    並列処理をしてマージ

--- pip-auto-update.sh
+++ pip-auto-update.sh
@@ -1,23 +1,69 @@
 . ./.env
 
-# 他にグローバル変数として下記のものが存在する。
-# 各サービスごとのパッケージのインストールエラーが発生した場合に、該当パッケージ名とバージョンの文字列を保存する配列
-# 変数名：{サービス名(ハイフンをアンダースコアへ変換)}_error_pkg_list。例）portfolio-appなら変数名はportfolio_app_error_pkg_list
+python_service_name_list=("portfolio-app" "finote-app" "shifree-app")
 
-# python_service_name_list=("portfolio-app" "finote-app" "shifree-app")
-python_service_name_list=("portfolio-app" "finote-app")
-is_rollback_list=()
-diff_text_list=()
+# 並列処理の結果を保存する一時ファイルを初期化する
+init_tmp_files() {
+    local file_path
+    for file_path in ${TMP_FILE_LIST[@]};do
+        : > $file_path
+   done
+}
+
+# 一時ファイルの削除
+remove_tmp_files() {
+    local file_path
+    for file_path in ${TMP_FILE_LIST[@]};do
+        rm -f $file_path
+    done
+}
+
+# 第一引数のフラグをもとに一時ファイルのパスを取得する
+# 引数：$1（is_rollback, diff_text, error_pkg）
+get_file_path() {
+    local flag=$1
+    local file_path
+
+    if [ $flag = "is_rollback" ]; then
+        file_path=${TMP_FILE_LIST[0]}
+    elif [ $flag = "diff_text" ]; then
+        file_path=${TMP_FILE_LIST[1]}
+    else
+        file_path=${TMP_FILE_LIST[2]}
+    fi
+
+    echo $file_path
+}
+
+# 指定された値を指定したファイルに書き込む
+# 引数　$1:flag, $2:index, $3:value
+write_csv() {
+    local file_path=$(get_file_path $1)
+    local index=$2
+    local value=$3
+    echo "$index,$value" >> $file_path
+}
+
+# 指定したファイル、index,カラム番号の値を取得する
+# 引数　$1:flag, $2:index, $3:column_number（0:行, 1:1番目, 2:2番目）
+get_value_from_csv() {
+    local file_path=$(get_file_path $1)
+    local index=$2
+    local column_num=$3
+    cat $file_path | awk -F , '$1 == '$index' {print '\$$column_num'}'
+}
 
 # 各サービスごとのパッケージ更新の差分のテキストを生成する。
 # rollbackした場合はrollbackとだけ出力する。
+# 引数　$1:差分の文字列（スペース区切り）, $2:index
 create_diff_text() {
     local diff_text=""
     local diff_list=$1
     local index=$2
     local diff
+    local is_rollback=$(get_value_from_csv is_rollback $index 2)
 
-    if [[ ${is_rollback_list[index]} = 1 ]]; then
+    if [[ $is_rollback = 1 ]]; then
         diff_text="${diff_text}\`rollback\`\n"
     else
         if [[ -n "${diff_list}" ]]; then
@@ -33,43 +79,18 @@ create_diff_text() {
     echo $diff_text
 }
 
-# 各サービスごとのパッケージインストールのエラー情報を保存するためのグローバル変数名を取得する。
-get_error_variable_name() {
-    local replaced_python_service_name=$(echo $1 | sed -e 's/-/_/g')
-    echo "${replaced_python_service_name}_error_pkg_list"
-}
-
-# 各サービスごとのパッケージのインストールエラー情報のテキストを生成する。
-create_error_pkg_info_text() {
-    local text=""
-    local error_variable_name=`get_error_variable_name "${python_service_name_list[index]}"`
-
-    local length=$(eval echo '${#'${error_variable_name}'[@]}')
-    if [ $length -ge 1 ]; then
-        text="${text}\`\`\`"
-
-        for error_pkg_info in `eval echo '${'${error_variable_name}'[@]}'`; do
-            text="${text}${error_pkg_info}\n"
-        done
-
-        text="${text}\`\`\`\n"
-    fi
-
-    echo $text
-}
-
 # 差分、エラー情報を通知用のテキストとして1つにまとめる。
 create_notification_text() {
     local text=""
     local index
-    local diff_text
 
     for index in "${!python_service_name_list[@]}"; do
-        local tmp_err_pkg_info_text=`create_error_pkg_info_text "$index"`
-        text="${text}*${python_service_name_list[index]}*\n"
-        text="${text}${tmp_err_pkg_info_text}"
+        local err_pkg_info_text=$(get_value_from_csv error_pkg $index 2)
+        local diff_text=$(get_value_from_csv diff_text $index 2)
 
-        text="${text}${diff_text_list[index]}\n\n"
+        text="${text}*\`${python_service_name_list[index]}\`*\n"
+        text="${text}${err_pkg_info_text}"
+        text="${text}${diff_text}\n\n"
     done
 
     echo $text
@@ -93,58 +114,84 @@ send_notification() {
 }
 
 main() {
-    for index in "${!python_service_name_list[@]}"; do
-        local docker_exec_command="docker exec ${python_service_name_list[index]}"
+    local index=$1
+    local outdated_pkg
+    local error_pkg_info_list=()
 
-        # 現在のインストール状態を一時保存（通知・pip check時のロールバック用）
-        local now_pkg_list=$(${docker_exec_command} pip list --format=freeze)
+    # for index in "${!python_service_name_list[@]}"; do
+    local docker_exec_command="docker exec ${python_service_name_list[index]}"
 
-        # サービスごとにエラーが起きたパッケージ名を保存
-        local error_variable_name=`get_error_variable_name "${python_service_name_list[index]}"`
+    # 現在のインストール状態を一時保存（通知・pip check時のロールバック用）
+    local now_pkg_list=$(${docker_exec_command} pip list --format=freeze)
 
-        ${docker_exec_command} pip install -U pip
+    ${docker_exec_command} pip install -U pip
 
-        local outdated_pkg_list=$(${docker_exec_command} pip list --outdated --format=freeze | awk -F "==" '{print $1}')
-        local outdated_pkg_list=(`echo $outdated_pkg_list`)
+    local outdated_pkg_list=$(${docker_exec_command} pip list --outdated --format=freeze | awk -F "==" '{print $1}')
+    local outdated_pkg_list=(`echo $outdated_pkg_list`)
 
-        for outdated_pkg in ${outdated_pkg_list[@]}; do
-            local now_version=$(${docker_exec_command} pip list | grep $outdated_pkg | awk -F " " '{print $2}')
-            local update_error=$(${docker_exec_command} pip install -U $outdated_pkg 2>&1 > /dev/null)
+    for outdated_pkg in ${outdated_pkg_list[@]}; do
+        local now_version=$(${docker_exec_command} pip list | grep $outdated_pkg | awk -F " " '{print $2}')
+        local update_error=$(${docker_exec_command} pip install -U $outdated_pkg 2>&1 > /dev/null)
 
-            # pip installでエラーが起きた場合はバージョンを戻す
-            if [ -n "$update_error" ]; then
-                ${docker_exec_command} pip install "${outdated_pkg}==${now_version}"
+        # pip installでエラーが起きた場合はバージョンを戻す
+        if [ -n "$update_error" ]; then
+            ${docker_exec_command} pip install "${outdated_pkg}==${now_version}"
 
-                local tmp_pkg_info="${outdated_pkg}==${now_version}--->ErrorDependency"
-                eval ${error_variable_name}+=\(\"${tmp_pkg_info}\"\)
-            fi
-        done
-
-        ${docker_exec_command} pip check
-        status=$?
-
-        # 依存関係が解消されない場合は全てを戻す
-        if [ $status = 1 ]; then
-            ${docker_exec_command} pip freeze | xargs pip uninstall -y
-            for now_pkg in ${now_pkg_list[@]}; do
-                ${docker_exec_command} pip install $now_pkg
-            done
-            is_rollback_list+=(1)
-            diff_text_list+=("NODIFF")
-        else
-            is_rollback_list+=(0)
-            local updated_pkg_list=$(${docker_exec_command} pip list --format=freeze)
-            local diff=$(diff -u <(echo "${now_pkg_list[@]}") <(echo "${updated_pkg_list[@]}") | grep -E '(^-\w|^\+\w)')
-            local tmp_diff=`create_diff_text "$diff" "$index"`
-
-            if [ -z "$tmp_diff" ]; then
-                diff_text_list+=("NODIFF")
-            else
-                diff_text_list+=($tmp_diff)
-            fi
+            local tmp_pkg_info="${outdated_pkg}==${now_version}--->ErrorDependency"
+            error_pkg_info_list+=($tmp_pkg_info)
         fi
     done
+
+    ${docker_exec_command} pip check
+    status=$?
+
+    # 依存関係が解消されない場合は全てを戻す
+    if [ $status = 1 ]; then
+        ${docker_exec_command} pip freeze | xargs pip uninstall -y
+        for now_pkg in ${now_pkg_list[@]}; do
+            ${docker_exec_command} pip install $now_pkg
+        done
+        write_csv is_rollback $index 1
+        write_csv diff_text $index \n\`\`\`NODIFF\`\`\`\n
+    else
+        write_csv is_rollback $index 0
+        local updated_pkg_list=$(${docker_exec_command} pip list --format=freeze)
+        local diff=$(diff -u <(echo "${now_pkg_list[@]}") <(echo "${updated_pkg_list[@]}") | grep -E '(^-\w|^\+\w)')
+        local tmp_diff=`create_diff_text "$diff" "$index"`
+
+        if [ -z "$tmp_diff" ]; then
+            write_csv diff_text $index \\n\`\`\`NODIFF\`\`\`\\n
+        else
+            write_csv diff_text $index $tmp_diff
+        fi
+
+        local error_pkg_info_text=""
+
+        # TODO: 関数として切り出し
+        if [ ${#error_pkg_info_list[@]} -ge 1 ]; then
+            error_pkg_info_text="${error_pkg_info_text}\`\`\`"
+
+            for error_pkg_info in ${error_pkg_info_list[@]}; do
+                error_pkg_info_text="${error_pkg_info_text}${error_pkg_info}\n"
+            done
+
+            error_pkg_info_text="${error_pkg_info_text}\`\`\`\n"
+        fi
+
+        write_csv error_pkg $index $error_pkg_info_text
+    fi
 }
 
-main
+init_tmp_files
+
+command=""
+for index in "${!python_service_name_list[@]}"; do
+    command="${command}main ${index}&"
+done
+
+eval $command
+wait
+
 send_notification $(create_notification_text)
+
+remove_tmp_files
