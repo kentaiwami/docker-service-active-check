0b2e39e75896690faeb3f2083fe2757eb2481ed0
Kenta Iwami <kentaiwami12@gmail.com>
2020-02-02 22:36:48 +0900

    http statusのチェックを導入

--- health-check.sh
+++ health-check.sh
@@ -1,27 +1,18 @@
 . ./.env
 
-service_name_list=("sumolog" "shifree" "portfolio" "portfolio-redirect" "finote" "phpmyadmin" "letsencrypt" "proxy")
+docker_service_names=("sumolog" "shifree" "portfolio" "portfolio-redirect" "finote" "phpmyadmin" "letsencrypt" "proxy")
+health_check_endpoint_service_names=(`echo ${docker_service_names[@]} | awk '{print $1}{print $2}{print $3}{print $4}{print $5}{print $6}'`)
 ok_list=()
 ng_list=()
 cause_list=()
-cause_docker_text="docker-process-is-down"
 
 send_notification() {
-    text=""
-
-    for ok_service_name in ${ok_list[@]}; do
-        text="${text}:white_check_mark: *${ok_service_name}* \n\n"
-    done
-
-    for index in "${!ng_list[@]}"; do
-        text="${text}:x: *${ng_list[$index]}* ${cause_list[$index]} \n\n"
-    done
-
-    channel=${channel:-'#health-check'}
-    botname=${botname:-'health-check'}
-    emoji=${emoji:-':heartpulse:'}
-    message=`echo ${text}`
-    payload="payload={
+    local text=$1
+    local channel=${channel:-'#health-check'}
+    local botname=${botname:-'health-check'}
+    local emoji=${emoji:-':heartpulse:'}
+    local message=`echo ${text}`
+    local payload="payload={
         \"channel\": \"${channel}\",
         \"username\": \"${botname}\",
         \"icon_emoji\": \"${emoji}\",
@@ -32,7 +23,8 @@ send_notification() {
 }
 
 run_docker_process_command() {
-    service_name=$1
+    local service_name=$1
+    local result_docker_process
 
     if [ $service_name = "portfolio" ]; then
         result_docker_process=$(docker ps -f "name=${service_name}" -f "status=running" --format "{{.Names}}\t{{.Status}}" | grep -v portfolio-redirect)
@@ -43,24 +35,73 @@ run_docker_process_command() {
     echo "$result_docker_process"
 }
 
-for index in "${!service_name_list[@]}"; do
-    service_name=${service_name_list[$index]}
-    result_docker_process=`run_docker_process_command ${service_name}`
-    running_service_count=$(echo "$result_docker_process" | wc -l)
+check_docker_container() {
+    local index
+    local cause_docker_text="docker-process-is-down"
 
-    # docker psの結果が空文字だったらng確定
-    if [ -z "$result_docker_process" ]; then
-        ng_list+=($service_name)
-        cause_list+=($cause_docker_text)
-    else
-        # 稼働中のサービスの個数が一致していなければng
-        if [[ $running_service_count = ${ACTIVE_SERVICE_COUNT_LIST[$index]} ]]; then
-            ok_list+=($service_name)
-        else
+    for index in "${!docker_service_names[@]}"; do
+        local service_name=${docker_service_names[$index]}
+        local result_docker_process=`run_docker_process_command ${service_name}`
+        local running_service_count=$(echo "$result_docker_process" | wc -l)
+
+        # docker psの結果が空文字だったらng確定
+        if [ -z "$result_docker_process" ]; then
             ng_list+=($service_name)
             cause_list+=($cause_docker_text)
+        else
+            # 稼働中のサービスの個数が一致していなければng
+            if [[ $running_service_count = ${ACTIVE_SERVICE_COUNT_LIST[$index]} ]]; then
+                ok_list+=($service_name)
+            else
+                ng_list+=($service_name)
+                cause_list+=($cause_docker_text)
+            fi
         fi
-    fi
-done
+    done
+}
 
-send_notification
+check_http_status() {
+    ok_list=()
+    ng_list=()
+    cause_list=()
+    local cause_http_status_text="http status is "
+
+    for index in "${!health_check_endpoint_service_names[@]}"; do
+        local http_status=(`curl -LI ${HEALTH_CHECK_URL[index]} -o /dev/null -w '%{http_code}\n' -s`)
+
+        if [ $http_status -eq 200 ]; then
+            ok_list+=(${health_check_endpoint_service_names[index]})
+        else
+            ng_list+=(${health_check_endpoint_service_names[index]})
+            cause_list+=("$cause_http_status_text $http_status")
+        fi
+    done
+}
+
+create_text() {
+    local text=""
+
+    for ok_service_name in ${ok_list[@]}; do
+        text="${text}:white_check_mark: *${ok_service_name}* \n\n"
+    done
+
+    for index in "${!ng_list[@]}"; do
+        text="${text}:x: *${ng_list[$index]}* ${cause_list[$index]} \n\n"
+    done
+
+    echo "$text"
+}
+
+main() {
+    # docker生存確認
+    check_docker_container
+    local docker_container_status_text=$(create_text)
+
+    # http status確認
+    check_http_status
+    local http_status_text=$(create_text)
+
+    send_notification "*\`docker container status\`*\n$docker_container_status_text\n*\`http status\`*\n$http_status_text"
+}
+
+main
